9520175dfa4cc306801451764c8d28eb4e4b3767
kebab <darknoobssquad@gmail.com>
2024-12-24 15:28:16 +0200

    master

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+
+    return(
+        <footer>
+            <ul>
+                <li>Cs:Go</li>
+                <li>Rust</li>
+            </ul>
+            <hr />
+        </footer>
+    )
+
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    
+    return(
+        <header>
+            <h1>Hello World</h1>
+            <hr />
+        </header>
+    )
+}
+
+export default Footer
